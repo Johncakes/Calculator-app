5cfad7b65e73f840e94eb4a264087dec8399ff26
John <johnnygamesover9000@gmail.com>
2025-02-24 10:56:26 +0900

    fix : display numbers overflowing at sm screen size

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,11 +5,11 @@ export default function Display(props: {
 }) {
   return (
     <div className="h-1/3 bg-white flex flex-col justify-between p-2 rounded-sm">
-      <div className="flex flex-row-reverse text-slate-600 text-4xl">
+      <div className="flex flex-row-reverse text-slate-600 text-3xl">
         {props.firstValue}
         {props.operator}
       </div>
-      <div className="flex flex-row-reverse break-words text-8xl bg-white text-black h-1/3 items-end">
+      <div className="flex flex-row-reverse break-words sm:text-6xl text-8xl bg-white text-black h-1/3 items-end">
         {props.displayValue}
       </div>
     </div>
